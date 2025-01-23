64feb39a21283248a98b36abb4a2217700ddf6d5
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2025-01-23 19:23:38 +0100

    feat: index route complete

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,7 +1,23 @@
 const connection = require('../data/db');
 
-const index = () => {
+const index = (req, res) => {
 
+    const sql = "SELECT * FROM `movies`;"
+
+    connection.query(sql, (err, results) => {
+
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            })
+        } else {
+            return res.status(200).json({
+                status: 'success',
+                data: results,
+            })
+        }
+        
+    })
 }
 
 const show = () => {

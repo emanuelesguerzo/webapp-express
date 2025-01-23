c55c08356631f2f3649060ddeed1486b7093d546
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2025-01-23 19:51:17 +0100

    feat: show route complete

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,8 +1,9 @@
 const connection = require('../data/db');
 
+// INDEX
 const index = (req, res) => {
 
-    const sql = "SELECT * FROM `movies`;"
+    const sql = 'SELECT * FROM `movies`;'
 
     connection.query(sql, (err, results) => {
 
@@ -16,12 +17,57 @@ const index = (req, res) => {
                 data: results,
             })
         }
-        
+
     })
 }
 
-const show = () => {
-    
+// SHOW
+const show = (req, res) => {
+
+    const id = req.params.id;
+    const sql = 'SELECT * FROM `movies` WHERE id = ?;';
+    const sqlReviews = `
+    SELECT reviews.* 
+    FROM reviews
+    JOIN movies
+    ON movies.id = reviews.movie_id
+    WHERE movies.id = ?;`
+
+    connection.query(sql, [id], (err, movies) => {
+
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            })
+        }
+
+        if (movies.length === 0) {
+            return res.status(404).json({
+                status: "fail",
+                message: "Movie Not Found",
+            });
+        }
+
+        connection.query(sqlReviews, [id], (err, reviews) => {
+
+            if (err) {
+                return res.status(500).json({
+                    message: 'Internal Server Error'
+                })
+            } else {
+                return res.status(200).json({
+                    status: 'success',
+                    data: {
+                        ...movies[0],
+                        reviews,
+                    }
+                })
+            }
+
+        })
+        
+    })
+
 }
 
 module.exports = {
